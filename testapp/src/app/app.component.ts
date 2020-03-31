018ea15be80c99bbf8b65ba346ac66fef1a14e88
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-31 19:26:36 +0300

    generate a component for service

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  subjects = [{title:'test'}];
+  subjects = [{title:'test'},{title:'test2'}];
+  
 }
