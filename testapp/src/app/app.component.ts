542725d673b82650b7ad9688935093681226d116
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-04-01 13:41:35 +0300

    created getSubjects method to finf all the subjects from the admin

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  subjects = [{title:'test'},{title:'test2'}];
-  
+  subjects = [{title:'test'}];
+
+  constructor(private api: ApiService) {
+    this.getSubjects();
+    
+  }
+  getSubjects= ()=> {
+    this.api.getAllSubjects().subscribe(
+      data => {
+     this.subjects=data;
+      },
+    error => {
+      console.log(error);
+    }
+    );
+  }
 }
